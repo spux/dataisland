924c21c017727c34d8dcdb4e772c64792efcb9a9
Melvin Carvalho <melvincarvalho@gmail.com>
2020-05-21 16:49:19 +0200

    add exports

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function dataIsland () {
+export default function dataIsland () {
   return JSON.parse(document.querySelector('[type="application/ld+json"]').text)
 }
 
-function dataIslands () {
+export function dataIslands () {
   return Array.from(document.querySelectorAll('[type="application/ld+json"]'))
     .map(island => [island.id, JSON.parse(island.text)])
     .reduce((obj, item) => {
@@ -11,12 +11,12 @@ function dataIslands () {
     }, {})
 }
 
-function updateIsland (id, data) {
+export function updateIsland (id, data) {
   document.getElementByID(id).text = JSON.stringify(data, null, 2)
   saveIsland(id)
 }
 
-function saveIsland (id) {
+export function saveIsland (id) {
   var island = document.getElementByID(id)
   var url = data.src
   var thejson = data.text
