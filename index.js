957f961e1add7a73ca5fd1e05b0a5c8dc62dc935
Melvin Carvalho <melvincarvalho@gmail.com>
2020-05-21 17:59:30 +0200

    add save island

--- index.js
+++ index.js
@@ -17,8 +17,18 @@ export function updateIsland (id, data) {
 }
 
 export function saveIsland (id) {
-  var island = document.getElementByID(id)
-  var url = data.src
-  var thejson = data.text
-  // PUT thejson to the src
+  var island = document.getElementById(id)
+  fetch(island.src, {
+    method: 'PUT', // or 'PUT'
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: island.text
+  })
+    .then(data => {
+      console.log('Success:', data)
+    })
+    .catch(error => {
+      console.error('Error:', error)
+    })
 }
