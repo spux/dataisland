9d904e4e1fbea6f911d36d362e6c1770d60fbb3c
Melvin Carvalho <melvincarvalho@gmail.com>
2020-05-21 18:45:26 +0200

    add auto update

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-let updater = {
-  set: (obj, prop, value) => {
-    obj[prop] = value
-    updateIsland(prop, value)
-    return true
-  }
+function saveIsland (id) {
+  var island = document.getElementById(id)
+  fetch(island.src, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: island.text
+  })
+    .then(data => {
+      console.log('Success:', data)
+    })
+    .catch(error => {
+      console.error('Error:', error)
+    })
 }
+
+;(() => {
+  globalThis.di = new Proxy(
+    Array.from(document.querySelectorAll('[type="application/ld+json"]'))
+      .map(island => [island.id, JSON.parse(island.text)])
+      .reduce((obj, item) => {
+        obj[item[0]] = item[1]
+        return obj
+      }, {}),
+    {
+      set: (obj, prop, value) => {
+        obj[prop] = value
+        document.getElementById(prop).text = JSON.stringify(value, null, 2)
+        saveIsland(prop)
+        return true
+      }
+    }
+  )
+})()
 
 export function dataIsland () {
   return JSON.parse(document.querySelector('[type="application/ld+json"]').text)
@@ -20,26 +48,4 @@ export function dataIslands () {
       }, {}),
     updater
   )
-}
-
-export function updateIsland (id, data) {
-  document.getElementById(id).text = JSON.stringify(data, null, 2)
-  saveIsland(id)
-}
-
-export function saveIsland (id) {
-  var island = document.getElementById(id)
-  fetch(island.src, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: island.text
-  })
-    .then(data => {
-      console.log('Success:', data)
-    })
-    .catch(error => {
-      console.error('Error:', error)
-    })
 }
